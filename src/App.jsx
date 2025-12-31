f424654d41c2f8e656b311f261f71d2d2515f0b4
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-31 12:11:02 +0300

    About Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home'
+import About from './pages/About';
 
 import './App.css'
 
@@ -15,7 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             {/* Uncomment as we create pages */}
-            {/* <Route path="/about" element={<About />} /> */}
+            <Route path="/about" element={<About />} />
             {/* <Route path="/services" element={<Services />} /> */}
             {/* <Route path="/portfolio" element={<Portfolio />} /> */}
             {/* <Route path="/blog" element={<Blog />} /> */}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,25 @@
-im
+import React from "react";
+import PageHeader from "../components/common/PageHeader";
+import CompanyOverview from "../components/sections/CompanyOverview";
+import VisionMission from "../components/sections/VisionMission";
+import CoreValues from "../components/sections/CoreValues";
+import Team from "../components/sections/Team";
+import TechStack from "../components/sections/TechStack";
+
+const About = () => {
+    return(
+        <div className="about-page">
+            <PageHeader 
+                title="About Us"
+                subtitle="Discover our story, meet our team, and learn about our values"
+            />
+            <CompanyOverview />
+            <VisionMission />
+            <CoreValues />
+            <Team />
+            <TechStack />
+        </div>
+    );
+};
+
+export default About;
